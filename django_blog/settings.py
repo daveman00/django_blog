55b932588a5deaf7b3dd5f87b14816bec6f8fb53
daveman00 <czyrny.dawid@gmail.com>
2019-09-24 14:26:35 +0200

    blog basic wireframe

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'm2g0bbeil#^1d&uc70kka6=9%c93$m(j*)yb92lrsl@wd@rtf^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'pl-pl'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Europe/Warsaw'
 
