3b04bc6f41494899e68f0eb0b8076446034e104d
daveman00 <czyrny.dawid@gmail.com>
2019-09-24 15:04:59 +0200

    improve admin object view

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return 'Category: ' + self.name
+
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -14,6 +17,9 @@ class Post(models.Model):
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField('Category', related_name='posts')
 
+    def __str__(self):
+        return 'Post: ' + self.title
+
 
 class Comment(models.Model):
     author = models.CharField(max_length=60)
@@ -21,3 +27,5 @@ class Comment(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return 'Comment: ' + self.author + ' - ' + self.body[:20] + '...'
